5d83eee5de89462147e0f716768d9fe32b3db8f6
LimBell <nowwhd987@naver.com>
2024-07-03 00:23:50 +0900

    사용되지 않는 CNN 관련 소스코드 복원 maxpool 미구현 cnn 작동 미확인

--- dnn/layer.py
+++ dnn/layer.py
@@ -26,3 +26,41 @@ class Linear(Updatable):
 
 class LayerNorm():
     pass
+
+
+# 
+class Conv3x3(Updatable):
+    def init(self, num_filters=8, name = ''):
+        filter_size=3
+        stride=1
+        stddev = cp.sqrt(1/9)
+        
+        # input size, output size를 입력받는 메커니즘을 삭제해야 conv layer를 구현할 수 있음
+        input_size = (123, 123)
+        output_size = (123 -2, 123 -2, num_filters)
+        
+        super().__init__(f'Conv{str(filter_size)}x{str(filter_size)}, filter*{str(num_filters)}) {name}', 
+                         input_size, 
+                         output_size,
+                         weight = stddev * cp.random.randn(filter_size, filter_size, num_filters), 
+                         bias = None)   
+        self.stride = stride
+        
+    def forward(self, x):
+        self.x = x
+        y = cp.zeros(self.output_size)
+        for i in self.output_size[0]:
+            for j in self.output_size[1]:
+                for filter in self.output_size[2]:
+                    y[i, j, filter] = cp.sum(x[i:i+3, j:j+3] * self.weight[:, :, filter])
+        return y
+    
+    def backward(self, d_y):
+        self.d_W = cp.zeros_like(self.weight)
+        d_x = cp.zeros_like(self.x)
+        for i in self.output_size[0]:
+            for j in self.output_size[1]:
+                for filter in self.output_size[2]:
+                    self.d_W[:, :, filter] += cp.sum(x[i:i+3, j:j+3] * d_y[i, j, filter])
+                    d_x[i:i+3, j:j+3] += self.weight[:, :, filter] * d_y[i, j, filter]
+        return d_x
